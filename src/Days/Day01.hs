15ca749b9342a95283e68f2f0801781787aba2ba
SamBobBarnes <sbarnes@vermeer.com>
2026-07-17 09:37:07 -0500

    wip learning

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,17 +1,35 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Days.Day01
-  ( solution
-  ) where
+  ( solution,
+  )
+where
 
 import Day (Day (..))
+import Debug.Trace (trace)
 
 solution :: Day
-solution = Day
-  { part1 = p1
-  , part2 = p2
-  }
+solution =
+  Day
+    { part1 = p1,
+      part2 = p2
+    }
 
 p1 :: String -> String
-p1 s = s ++ "!"
+p1 s =
+  let arr = getPairs s
+   in trace
+        ("getPairs=" ++ show arr)
+        s
+        let arr2 = take (length arr - 1) arr
+         in trace ("arr2=" ++ show arr2) s
+
+getPairs :: [Char] -> [[Char]]
+getPairs (s : xs) = getFirstPair 0 2 (s : xs) : getPairs xs
+getPairs [] = []
+
+getFirstPair :: Int -> Int -> [Char] -> [Char]
+getFirstPair start end xs = take (end - start) (drop start xs)
 
 p2 :: String -> String
 p2 s = s ++ "?"
