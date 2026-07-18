67a90cd1bf3f6dcdc6530b51b9807741a1f05692
SamBobBarnes <sbarnes@vermeer.com>
2026-07-18 13:20:29 -0500

    d1p1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -15,11 +15,24 @@ solution =
       part2 = p2
     }
 
+-- Part 1
 p1 :: String -> String
-p1 s = s
+p1 s = show $ countMatchingPairs s 0 0
 
-countMatchingPairs :: [Char] -> Int -> Int
-countMatchingPairs [] _ = 0
+-- ([Char] xs, Int count, Int i) -> Int
+countMatchingPairs :: [Char] -> Int -> Int -> Int
+countMatchingPairs [] _ _ = 0
+countMatchingPairs xs count i =
+  if i == ((length xs) - 1)
+    then
+      if doAAndBMatch xs i 0
+        then count + (getValOfChar (xs !! i))
+        else count
+    -- return count
+    else
+      if doAAndBMatch xs i (i + 1) -- recurse
+        then countMatchingPairs xs (count + (getValOfChar (xs !! i))) (i + 1)
+        else countMatchingPairs xs count (i + 1)
 
 {- pseudocode
 for i in 0..length(xs) - 1:
@@ -29,7 +42,6 @@ for i in 0..length(xs) - 1:
     b = 0
   if doAAndBMatch(xs, i, i + 1):
       count += 1
-
 -}
 
 doAAndBMatch :: [Char] -> Int -> Int -> Bool
@@ -39,5 +51,9 @@ doAAndBMatch xs a b
   | a >= length xs || b >= length xs = False
   | otherwise = (xs !! a) == (xs !! b)
 
+getValOfChar :: Char -> Int
+getValOfChar c = (fromEnum c) - (fromEnum '0')
+
+-- Part 2
 p2 :: String -> String
 p2 s = s ++ "?"
